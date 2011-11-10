935de1cc19a128d8c4e86e97003a354f8d2fb744
Kushal Das <kdas@redhat.com>
2011-11-10 11:13:43 +0530

    Changed the section name to darkserverweb.

--- darkserverweb/settings.py
+++ darkserverweb/settings.py
@@ -129,10 +129,10 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 #logging settings
-LOG_FILENAME = 'darkserver.log'
-logging.basicConfig(
-    filename=os.path.join('/var', 'log', 'darkserver', LOG_FILENAME),
-    level=logging.CRITICAL,
-    format='%(pathname)s TIME: %(asctime)s MSG: %(filename)s:%(funcName)s:%(lineno)d %(message)s',
-)
+#LOG_FILENAME = 'darkserver.log'
+#logging.basicConfig(
+#    filename=os.path.join('/var', 'log', 'darkserver', LOG_FILENAME),
+#    level=logging.CRITICAL,
+#    format='%(pathname)s TIME: %(asctime)s MSG: %(filename)s:%(funcName)s:%(lineno)d %(message)s',
+#)
 
