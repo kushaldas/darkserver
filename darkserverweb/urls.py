10db00a6cecff1221a3c649d3622bb18b41a706d
Kushal Das <kdas@redhat.com>
2011-11-10 11:06:34 +0530

    Removed u , fixed the typo

--- darkserverweb/urls.py
+++ darkserverweb/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(u(r'^static/(?P<path>.*)$'),
+    url(r'^static/(?P<path>.*)$',
         'django.views.static.serve', {
             'document_root': MEDIA_ROOT,
         },  
