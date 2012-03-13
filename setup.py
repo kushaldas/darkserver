c09bbfd150cf9ca037fa3bcef53facf6222c582a
Kushal Das <kushaldas@gmail.com>
2012-03-14 00:20:02 +0530

    koji-plugin in setup.py and MANIFEST.in
    Description
    ===========
    Now setup.py installs the koji-plugin.

--- setup.py
+++ setup.py
@@ -7,18 +7,20 @@ import os
 
 
 setup(name='darkserver',
-      version='0.4.1',
+      version='0.5',
       description="GNU build-id web service",
       long_description = "GNU build-id web service",
       platforms = ["Linux"],
       author="Kushal Das",
-      author_email="kdas@redhat.com",
+      author_email="kushaldas@gmail.com",
       url="https://github.com/kushaldas/darkserver",
       license = "http://www.gnu.org/copyleft/gpl.html",
       packages = ['darkserverweb','darkserverweb.buildid'],
       data_files=[('/etc/httpd/conf.d/', ['darkserver-httpd.conf']),
           ('/usr/sbin/', ['darkserver.wsgi']),
           ('/etc/darkserver/', ['darkserverweb/settings.py','darkserverweb.conf']),
+          ('/etc/koji-hub/plugins/', ['koji-plugin/darkserver.conf']),
+          ('/usr/lib/koji-hub-plugins/', ['koji-plugin/darkserver-plugin.py']),
           ('/usr/share/darkserver/static', ['static/index.html','static/style.css']),]
 
       )
