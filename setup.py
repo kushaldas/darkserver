f68686153a011839d58c2a06dfe0047affe47a24
Kushal Das <kushaldas@gmail.com>
2013-04-02 22:55:20 +0530

    moved all config files into configs directory

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ setup(name='darkserver',
       author_email="kushaldas@gmail.com",
       url="https://github.com/kushaldas/darkserver",
       license = "http://www.gnu.org/copyleft/gpl.html",
-      packages = ['darkserverweb', 'darkserverweb.buildid', 'darkimporter'],
-      data_files=[('/etc/httpd/conf.d/', ['darkserver-httpd.conf']),
+      packages = ['darkserverweb', 'darkserverweb.buildid', 'configs/darkimporter'],
+      data_files=[('/etc/httpd/conf.d/', ['configs/darkserver-httpd.conf']),
           ('/usr/sbin/', ['darkserver.wsgi', 'darkbuildqueue',\
                           'darkjobworker', 'darkproducer', 'darkdashboard']),
-          ('/etc/darkserver/', ['darkserverweb/settings.py', 'darkserverweb.conf',\
-                                'data/dark-distros.json', 'data/darkjobworker.conf',\
-                                'data/redis_server.json', 'data/email.json']),
+          ('/etc/darkserver/', ['darkserverweb/settings.py', 'configs/darkserverweb.conf',\
+                                'configs/dark-distros.json', 'configs/darkjobworker.conf',\
+                                'configs/redis_server.json', 'configs/email.json']),
           ('/usr/share/darkserver/static', ['static/index.html', 'static/404.html', \
                                             'static/500.html']), ]
 
