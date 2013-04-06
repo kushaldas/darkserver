3c34c4f63bac259ae09a97903b2b8aa2687fddca
Kushal Das <kushaldas@gmail.com>
2013-04-06 21:48:56 +0530

    tests send_mail function from utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ import logging
 imppath = os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(0, imppath)
 from darkimporter.utils import log, send_mail
+from mock import patch
 
 
 logging.basicConfig(level=logging.INFO,\
@@ -13,3 +14,8 @@ def test_log(caplog):
     log('mylog', 'test message', 'info')
     with caplog.atLevel(logging.INFO):
         assert caplog.text() == 'utils.py                    42 INFO     test message\n'
+
+def test_send_email():
+    with patch('darkimporter.utils.Popen') as mock_popen:
+        mock_popen.communicate.return_value = True
+        assert send_mail('kushaldas@gmail.com','d@das.com','subject', 'test email')
