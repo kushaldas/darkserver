b80423971f4cd1edccb7ad15c056d2d0d42e3db8
Kushal Das <mail@kushaldas.in>
2017-03-01 17:06:17 +0530

    Updates the setup.py to 2.0 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='darkserver',
-    version='1.0.4',
+    version='2.0',
     description="GNU build-id web service",
     long_description = "GNU build-id web service",
     platforms = ["Linux"],
@@ -12,15 +12,11 @@ setup(
     author_email="mail@kushaldas.in",
     url="https://github.com/kushaldas/darkserver",
     license = "http://www.gnu.org/copyleft/gpl.html",
-    packages = ['darkserverweb', 'darkserverweb.buildid', 'darkimporter', 'darkserver'],
+    packages = ['darkweb', 'darkserverweb.buildid', 'darkimporter', 'darkserver'],
     data_files=[('/etc/httpd/conf.d/', ['darkserver-httpd.conf']),
-        ('/usr/sbin/', ['darkserver.wsgi', 'darkdashboard']),
-        ('/etc/darkserver/', ['darkserverweb/settings.py', 'darkserverweb.conf',\
-                            'data/koji_info.json', 'data/darkjobworker.conf',\
-                            'data/redis_server.json', 'data/email.json']),
-        ('/usr/share/darkserver/', ['darkjobworker.py']),
-        ('/usr/share/darkserver/static', ['static/index.html', 'static/404.html', \
-                                        'static/500.html']), ],
+        ('/usr/sbin/', ['darkserver.wsgi',]),
+        ('/etc/darkserver/', ['data/koji_info.json', 'data/darkjobworker.conf',]),
+        ('/usr/share/darkserver/', ['darkjobworker.py']),],
     entry_points={
         'moksha.consumer': [
             "darkserver.consumer = darkserver.consumer:DarkserverConsumer",
